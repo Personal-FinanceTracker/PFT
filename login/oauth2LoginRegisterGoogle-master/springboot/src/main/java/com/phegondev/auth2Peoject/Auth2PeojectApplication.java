70574066d3824a4ae18830845cbb1836035e505e
prasanjanbiswal <prasanjanbiswal@gmail.com>
2025-07-09 21:43:55 +0530

    heee

---
+++ login/oauth2LoginRegisterGoogle-master/springboot/src/main/java/com/phegondev/auth2Peoject/Auth2PeojectApplication.java
@@ -0,0 +1,14 @@
+package com.phegondev.auth2Peoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication
+public class Auth2PeojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth2PeojectApplication.class, args);
+	}
+
+}
---
+++ login/oauth2LoginRegisterGoogle-master/springboot/src/main/java/com/phegondev/auth2Peoject/controller/AuthController.java
@@ -0,0 +1,56 @@
+package com.phegondev.auth2Peoject.controller;
+
+import com.phegondev.auth2Peoject.enums.AuthProvider;
+import com.phegondev.auth2Peoject.model.User;
+import com.phegondev.auth2Peoject.repository.UsersRepository;
+import com.phegondev.auth2Peoject.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URI;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        return ResponseEntity.ok(userService.registerUserLocal(user));
+
+    }
+
+    @PostMapping("/login/local")
+    public ResponseEntity<User> loginLocal(@RequestBody User user) {
+        return ResponseEntity.ok(userService.loginUserLocal(user));
+    }
+
+    @GetMapping("/login/google")
+    public ResponseEntity<String> loginGoogleAuth(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/oauth2/authorization/google");
+        return ResponseEntity.ok("Redirecting ..");
+    }
+
+    @GetMapping("/loginSuccess")
+    public ResponseEntity<?> handleGoogleSuccess(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        User user = userService.loginRegisterByGoogleOAuth2(oAuth2AuthenticationToken);
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create("http://localhost:3000/home")).build();
+    }
+
+}
